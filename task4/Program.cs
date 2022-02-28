d98dcf0a0d19ce031b2bf8fcd6caf45ff16c7c4e
Nikita Saburov <saburov333@mail.ru>
2022-02-28 21:26:46 +0500

    task4 ready

---
+++ task4/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max;
+
+if (number1 > number2)
+{
+    max = number1;
+}
+else
+{
+    max = number2;
+}
+if (max > number3)
+{
+    Console.WriteLine("максимальное число " + max);
+}
+else
+{
+    Console.WriteLine("максимальное число " + number3);
+}
