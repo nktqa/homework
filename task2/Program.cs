b71784cced0b4357ca91c1335ee6972850c33c26
Nikita Saburov <saburov333@mail.ru>
2022-02-28 21:23:42 +0500

    task2 ready

---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine("максимальное число " + number1);
+    Console.WriteLine("минимальное число " + number2);
+}
+else
+{
+    Console.WriteLine("максимальное число " + number2);
+    Console.WriteLine("минимальное число " + number1);
+}
