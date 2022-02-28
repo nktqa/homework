afbd0038e934b188d4b991be0a5ee5db15a629eb
Nikita Saburov <saburov333@mail.ru>
2022-02-28 21:29:44 +0500

    task 6 ready

---
+++ task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("число четное");
+}
+else
+{
+    Console.WriteLine("число не четное");
+}
