9bef4011bb947f6c26ba375f24dc010f060eb2a1
Nikita Saburov <saburov333@mail.ru>
2022-03-15 19:56:55 +0500

    сделал задание 15

---
+++ task15/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.*/
+int number = new Random().Next(1,8);
+Sleep(number);
+
+
+void Sleep(int currentNumber)
+{
+    if(currentNumber <= 5)
+    {
+        Console.WriteLine($"{currentNumber} это рабочий день");
+    }
+    else
+    {
+        Console.WriteLine($"{currentNumber} это ВЫХОДНОЙ!");
+    }
+}
