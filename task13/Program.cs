8003067f3bcdc1ee4e8aeec0e5a92f47119f2aab
Nikita Saburov <saburov333@mail.ru>
2022-03-15 19:45:44 +0500

    сделал 13 задание

---
+++ task13/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+или сообщает, что третьей цифры нет.*/
+int number = new Random().Next(1,99999);
+raschet(number);
+
+void raschet(int current_number)
+{
+    if (current_number > 99)
+    {
+        Console.WriteLine($"третьей цифрой числа {current_number} является цифра {number.ToString()[2]}");
+    }
+    else
+    {
+        Console.WriteLine($"у числа {current_number} нет третьей цифры");
+    }
+}
