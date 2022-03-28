efa72a591c593bbd91acafbb63d81b6b0b7b78b9
Nikita Saburov <saburov333@mail.ru>
2022-03-28 20:20:14 +0500

    сделал задание 19

---
+++ task19/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 19: Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("введите пятизначное число: ");
+string number = Console.ReadLine();
+
+if (number[0] == number[4] && number[1] == number[3])
+{
+    Console.WriteLine("это число - паралиндром");
+}
+else
+{
+    Console.WriteLine("это чисто не палиндром");
+}
