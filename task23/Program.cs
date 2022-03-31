0cecd4977f5a340dbf0885fcfca325f48ad21dcd
Nikita Saburov <saburov333@mail.ru>
2022-03-31 17:52:06 +0500

    сделал задание 23

---
+++ task23/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 23: Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+Random rand = new Random();
+int number = rand.Next(1, 999);
+Console.WriteLine(number);
+
+for (int i = 1; i < number; i++)
+{
+    double rezultat = Math.Pow(i, 3);
+    Console.WriteLine(rezultat);
+}
