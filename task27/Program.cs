8be0c8bfb7394c7b3371635ff1e0b513823d917f
Nikita Saburov <saburov333@mail.ru>
2022-04-06 21:18:43 +0500

    сделал задачу 27

---
+++ task27/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+
+Console.WriteLine("введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int chislo = 1;
+int summa = 0;
+
+while (number > 0)
+{
+    chislo = number % 10;
+    number /= 10;
+    summa += chislo;
+}
+Console.WriteLine(summa);
