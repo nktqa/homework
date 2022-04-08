a667161731dfaee6cfee99a2ca8039ab35b75345
Nikita Saburov <saburov333@mail.ru>
2022-04-08 15:02:08 +0500

    сделал задачу 29

---
+++ task29/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 случайных целых чисел 
+// и выводит отсортированный по модулю массив.
+
+Random rand = new Random();
+int[] array = new int[8];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(-5, 5);
+    Console.Write(Math.Abs(array[i]));
+    Console.Write(" ");
+}
