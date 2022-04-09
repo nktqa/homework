e3572af9166d47af4c69dbd678bda8f768dfc2a7
Nikita Saburov <saburov333@mail.ru>
2022-04-09 13:51:43 +0500

    переделал задачу 29

--- task29/Program.cs
+++ task29/Program.cs
@@ -6,7 +6,29 @@ int[] array = new int[8];
 
 for (int i = 0; i < array.Length; i++)
 {
-    array[i] = rand.Next(-5, 5);
-    Console.Write(Math.Abs(array[i]));
-    Console.Write(" ");
+    array[i] = rand.Next(-50, 50);
+}
+
+BubbleSort(array);
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(Math.Abs(array[i]) + " ");
+}
+
+
+void BubbleSort(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        for (int j = 0; j < array.Length - 1; j++)
+        {
+            if (Math.Abs(array[j]) > Math.Abs(array[j + 1]))
+            {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
 }
