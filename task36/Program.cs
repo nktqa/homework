87ba7d0c6c57363f2fc7370ece328c91670bbbeb
Nikita Saburov <saburov333@mail.ru>
2022-04-29 16:07:30 +0500

    сделал задание 36

---
+++ task36/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] array = new int[7];
+FillArray();
+PrintArray();
+Search();
+
+void Search()
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0) count += array[i];
+    }
+    Console.Write(count);
+}
+
+void FillArray()
+{
+    Random rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(1, 10);
+    }
+    Console.WriteLine();
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
