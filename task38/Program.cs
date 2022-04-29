e55f8c000f1b303570b554ac7f22af690f4ab156
Nikita Saburov <saburov333@mail.ru>
2022-04-29 18:09:20 +0500

    сделал задание 38

---
+++ task38/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между 
+// максимальным и минимальным элементов массива.
+
+double[] array = new double[7];
+FillArray();
+PrintArray();
+Search();
+
+void Search()
+{
+    double maxNum = array[0];
+    double minNum = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (maxNum < array[i])
+        {
+            maxNum = array[i];
+        }
+        if (minNum > array[i])
+        {
+            minNum = array[i];
+        }
+    }
+    Console.WriteLine($"макс {maxNum}, мин {minNum}");
+    Console.Write($"раздница между {maxNum} и {minNum} = {maxNum - minNum}");
+}
+
+void FillArray()
+{
+    Random rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.NextDouble() * 100;
+    }
+    Console.WriteLine();
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
