eee3b0b3a704b1276c237e47877a89660ea8f8c8
Nikita Saburov <saburov333@mail.ru>
2022-05-02 19:24:58 +0500

    сделал задание 47

---
+++ task47/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 47: Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+
+int rows = 3;
+int columns = 4;
+
+double[,] array = new double[rows, columns];
+FillArray();
+PrintArray();
+
+void FillArray()
+{
+    Random rand = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i,j] = rand.NextDouble() * 100;
+        }
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
