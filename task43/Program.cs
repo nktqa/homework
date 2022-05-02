6b7f64b2cd1d36ae128cad2b09b382885e123852
Nikita Saburov <saburov333@mail.ru>
2022-05-02 14:45:52 +0500

    переделал задачу 43

--- task43/Program.cs
+++ task43/Program.cs
@@ -3,11 +3,11 @@
 // значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-int b1 = 2;
-int k1 = 5;
-int b2 = 4;
-int k2 = 9;
+double b1 = 2;
+double k1 = 5;
+double b2 = 4;
+double k2 = 9;
 
-int x = (b2 - b1) / (k1 - k2);
+double x = (b2 - b1) / (k1 - k2);
 
 Console.Write($"точка пересечения {x} : {k1*x+b1}");
